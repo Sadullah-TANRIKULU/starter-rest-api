e5006cc7b263b4e62c29336639e450c5c2f59f8f
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-12-20 22:47:51 +0300

    dynamoDB added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const db = require('cyclic-dynamodb')
+const CyclicDb = require("@cyclic.sh/dynamodb")
+const db = CyclicDb("alert-gray-hippoCyclicDB")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
